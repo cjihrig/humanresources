cf4c450f4a11808d3af1df2e4ffbba55738cc867
Adam Bretz <arbretz@gmail.com>
2014-08-24 22:07:14 -0400

    Fixed spacing.

--- assets/javascript/03_employee_controller.js
+++ assets/javascript/03_employee_controller.js
@@ -1,75 +1,75 @@
 app.controller('EmployeesCtrl', ['$scope', 'EmployeeService', function($scope, service) {
-	service.query(function(data, headers) {
-		$scope.employees = data;
-	}, _handleError);
+  service.query(function(data, headers) {
+    $scope.employees = data;
+  }, _handleError);
 }]);
 
 app.controller('EmployeeCtrl', ['$scope', '$routeParams', 'EmployeeService', 'TeamService', '$q', 'config', '$route',
-	function($scope, $routeParams, employee, team, $q, config, $route) {
+  function($scope, $routeParams, employee, team, $q, config, $route) {
 
-		$scope.address = {};
+    $scope.address = {};
 
-		function getTeam (teams, teamId) {
-			for (var i = 0, l = teams.length; i < l; ++i) {
-				var t = teams[i];
-				if (t._id === teamId) {
-					return t;
-				}
-			}
-		}
+    function getTeam (teams, teamId) {
+      for (var i = 0, l = teams.length; i < l; ++i) {
+        var t = teams[i];
+        if (t._id === teamId) {
+          return t;
+        }
+      }
+    }
 
-	$q.all([
-		employee.get({
-			employeeId: $routeParams.employeeId
-		}).$promise,
-		team.query().$promise
-	]).then(function(values) {
-		$scope.teams = values[1];
-		$scope.employee = values[0];
-		$scope.employee.team = getTeam($scope.teams, $scope.employee.team._id);
-	}).catch(_handleError);
+  $q.all([
+    employee.get({
+      employeeId: $routeParams.employeeId
+    }).$promise,
+    team.query().$promise
+  ]).then(function(values) {
+    $scope.teams = values[1];
+    $scope.employee = values[0];
+    $scope.employee.team = getTeam($scope.teams, $scope.employee.team._id);
+  }).catch(_handleError);
 
-	$scope.editing = false;
-	// to prevent multiple refereces to the same array, give us a new copy of it.
-	$scope.states = config.states.slice(0);
+  $scope.editing = false;
+  // to prevent multiple refereces to the same array, give us a new copy of it.
+  $scope.states = config.states.slice(0);
 
-	$scope.edit = function() {
-		$scope.editing = !$scope.editing;
-	};
+  $scope.edit = function() {
+    $scope.editing = !$scope.editing;
+  };
 
-	$scope.save = function() {
-		//to prevent empty lines geting to the database and also to keep the UI clean
-		// remove any blank lines
-		var lines = $scope.employee.address.lines;
+  $scope.save = function() {
+    //to prevent empty lines geting to the database and also to keep the UI clean
+    // remove any blank lines
+    var lines = $scope.employee.address.lines;
 
-		if (lines.length) {
-			lines = lines.filter(function (value) {
-				return value;
-			});
-		}
+    if (lines.length) {
+      lines = lines.filter(function (value) {
+        return value;
+      });
+    }
 
-		$scope.employee.address.lines = lines;
+    $scope.employee.address.lines = lines;
 
-		employee.update({
-			employeeId: $routeParams.employeeId
-		}, $scope.employee, function() {
-			$scope.editing = !$scope.editing;
-		});
-	};
+    employee.update({
+      employeeId: $routeParams.employeeId
+    }, $scope.employee, function() {
+      $scope.editing = !$scope.editing;
+    });
+  };
 
-	$scope.cancel = function () {
-		$route.reload();
-	}
+  $scope.cancel = function () {
+    $route.reload();
+  }
 
-	$scope.address.addLine = function (index) {
-		var lines = $scope.employee.address.lines;
+  $scope.address.addLine = function (index) {
+    var lines = $scope.employee.address.lines;
 
-		lines.splice(index + 1, 0, '');
-	}
+    lines.splice(index + 1, 0, '');
+  }
 
-	$scope.address.removeLine = function (index) {
-		var lines = $scope.employee.address.lines;
+  $scope.address.removeLine = function (index) {
+    var lines = $scope.employee.address.lines;
 
-		lines.splice(index, 1);
-	}
+    lines.splice(index, 1);
+  }
 }]);
--- assets/javascript/04_team_controller.js
+++ assets/javascript/04_team_controller.js
@@ -1,13 +1,13 @@
 app.controller('TeamsCtrl', ['$scope', 'TeamService', function($scope, service) {
-	service.query(function (data) {
-		$scope.teams = data;
-	}, _handleError);
+  service.query(function (data) {
+    $scope.teams = data;
+  }, _handleError);
 }]);
 
 app.controller('TeamCtrl', ['$scope', '$routeParams', 'TeamService', function($scope, $routeParams, service) {
-	service.get({
-		teamId: $routeParams.teamId
-	}, function(data, headers) {
-		$scope.team = data;
-	}, _handleError);
+  service.get({
+    teamId: $routeParams.teamId
+  }, function(data, headers) {
+    $scope.team = data;
+  }, _handleError);
 }]);
